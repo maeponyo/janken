db9a674f671fd23747a7b31b7dd73e57246dd4ad
Haruka Maeda <hmaeda@sfc.keio.ac.jp>
2017-05-25 12:12:52 +0900

    do lectre05

--- docs/player.js
+++ docs/player.js
@@ -1,21 +1,24 @@
 const GU = 0;
 const CHOKI = 1;
 const PA = 2;
-let matches;
 var ans;
-var list;
-var index;
-var lastCard;
 
-matches = 0;
+//デフォルト
+function randomCard(matches){
+  let randomValue = Math.random();
+  let randomInt = Math.floor(randomValue * 100);
+  let cardNum = Math.floor(randomInt / 3);
+  return cardNum;
+}
+
 
 // チョキ
-function actionAgainstChoki(){
+function actionAgainstChoki(matches){
   return GU;
 }
 
 // 裏か表か
-function actionAgainstOmoteUra(){
+function actionAgainstOmoteUra(matches){
   if(matches % 2 == 0){
     return GU;
   }
@@ -23,7 +26,7 @@ function actionAgainstOmoteUra(){
 }
 
 // 帰って来たうらか表か
-function actionAgainstOmoteUra2(){
+function actionAgainstOmoteUra2(matches){
   if(matches % 3 == 0){
     return CHOKI;
   }
@@ -31,7 +34,7 @@ function actionAgainstOmoteUra2(){
 }
 
 // 順番に出すマン
-function actionAgainstJunban(){
+function actionAgainstJunban(matches){
   if(matches % 3 == 1){
     return PA;
   }
@@ -44,23 +47,19 @@ function actionAgainstJunban(){
 }
 
 //帰って来た裏か表か
-function actionAgainstRotation2(matches){
-  if(matches == 1 || matches == 2){
-    return GU;
+function actionAgainstRotation2(matches,lastCard,lastCard2){
+  if(matches <= 2){
+    ans = randomCard();
+    return ans;
   }
-
-  let index2 = Math.max(0, list.length -2 );
-  let lastCard2 = list[index2];
-
   if(lastCard2 == lastCard + 1 || lastCard2 == lastCard - 2){
     return lastCard2;
   }
-
   return lastCard;
 }
 
 // フィズバズ
-function actionAgainstFizzBuzz(oppornent){
+function actionAgainstFizzBuzz(matches){
   if(matches % 15 == 0){
     return CHOKI;
   }
@@ -70,40 +69,42 @@ function actionAgainstFizzBuzz(oppornent){
   return PA;
 }
 
-// ランダム
-function actionAgainstRandom() {
+// ランダムマン
+function actionAgainstRandom(matches) {
   return GU;
 }
 
-
 function action(oppornent){
-
-  list = cards();
-  index = Math.max(0, list.length -1 );
-  lastCard = list[index];
-
-  matches = list.length + 1;
+  var list = cards();
+  var index = Math.max(0, list.length -1 );
+  var index2 = Math.max(0, list.length -2 );
+  var lastCard = list[index];
+  var lastCard2 = list[index2];
+  let matches = list.length + 1;
 
   if(oppornent == "fighter::choki-lover"){
-    ans = actionAgainstChoki();
+    ans = actionAgainstChoki(matches);
   }
   else if(oppornent === "fighter::odd-even"){
-    ans = actionAgainstOmoteUra();
+    ans = actionAgainstOmoteUra(matches);
   }
   else if(oppornent == "fighter::on-third"){
-    ans = actionAgainstOmoteUra2();
+    ans = actionAgainstOmoteUra2(matches);
   }
   else if(oppornent == "fighter::rotation"){
-    ans = actionAgainstJunban();
+    ans = actionAgainstJunban(matches);
   }
   else if(oppornent == "fighter::returned-rotation"){
-    ans = actionAgainstRotation2(matches);
+    ans = actionAgainstRotation2(matches,lastCard,lastCard2);
   }
   else if (oppornent == "fighter::fizzbuzz") {
-    ans = actionAgainstFizzBuzz();
+    ans = actionAgainstFizzBuzz(matches);
   }
-  else{
-    ans = actionAgainstRandom();
+  // else if (oppornent == "fighter::random"){
+  //   ans = actionAgainstRandom();
+  // }
+  else {
+    ans = randomCard(matches);
   }
   return ans;
 }
